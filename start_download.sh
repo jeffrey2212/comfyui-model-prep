60b4d709484686b30158d973af0e138b14d6f67b
jeffrey2212 <jeffrey2212@gmail.com>
2024-03-22 20:39:38 +0800

    fix the installation

--- start_download.sh
+++ start_download.sh
@@ -8,9 +8,13 @@ fi
 
 # Activate the virtual environment
 source venv/bin/activate
-
-# Install requirements
-pip install -r requirements.txt
+# Check if requirements are already installed
+if [ ! -f "requirements_installed.txt" ]; then
+    # Install requirements
+    pip install -r requirements.txt
+    # Create a file to mark that requirements are installed
+    touch requirements_installed.txt
+fi
 
 # Run main.py
 python main.py
